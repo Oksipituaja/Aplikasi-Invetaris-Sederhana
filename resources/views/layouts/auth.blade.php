464857035a765391c9b0b2dc823d25de95596f4a
Yusuf Hammam <oksipituaja@gmail.com>
2025-12-02 15:23:21 +0700

    Update auth.blade.php

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -8,6 +8,13 @@
     {{-- Bootstrap & Font Awesome CDN --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+      <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:400,600&display=swap">
+      <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+      <!-- Theme style -->
+    <link rel="stylesheet" href="https://aplikasi-invetaris-sederhan-vau69.sevalla.app/templates/dist/css/adminlte.min.css">
 
     {{-- Optional Custom Styling --}}
     <style>
