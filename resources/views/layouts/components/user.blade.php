326f2dcef8f3b16afbc1e8b104f722510673e551
Yusuf Hammam <oksipituaja@gmail.com>
2025-12-02 15:47:28 +0700

    Update user.blade.php

--- resources/views/layouts/components/user.blade.php
+++ resources/views/layouts/components/user.blade.php
@@ -15,7 +15,7 @@
       <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
       <!-- Theme style -->
-  <link rel="stylesheet" href="https://aplikasi-invetaris-sederhan-vau69.sevalla.app/templates/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="https://aplikasi-invetaris-sederhana.sevalla.app/templates/dist/css/adminlte.min.css">
 </head>
 <body class="hold-transition layout-top-nav">
 
