2001fbd45262cad6b23acca5d01c61730d8c6e05
Yusuf Hammam <oksipituaja@gmail.com>
2025-12-03 23:14:58 +0700

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,89 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Aplikasi Invetaris Sederhana</title>
-<script src="https://aplikasi-invetaris-sederhana.sevalla.app/templates/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 (CSS & JS) -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-
-<!-- Font Awesome (untuk ikon) -->
-<link rel="stylesheet" href="https://aplikasi-invetaris-sederhana.sevalla.app/templates/plugins/fontawesome-free/css/all.min.css">
-
-<!-- Tempus Dominus Bootstrap 4 -->
-<link rel="stylesheet" href="https://aplikasi-invetaris-sederhana.sevalla.app/templates/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-<script src="https://aplikasi-invetaris-sederhana.sevalla.app/templates/plugins/moment/moment.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment-with-locales.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.43/moment-timezone.min.js"></script>
-<script src="https://aplikasi-invetaris-sederhana.sevalla.app/templates/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Aplikasi Invetaris Sederhana</title>
+  
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
+      <!-- Font Awesome -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:400,600&display=swap">
-    <!-- Ionicons -->
+      <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Theme style -->
+      <!-- Theme style -->
     <link rel="stylesheet" href="https://aplikasi-invetaris-sederhana.sevalla.app/templates/dist/css/adminlte.min.css">
-
 </head>
-
 <body class="hold-transition sidebar-mini">
-    <div class="wrapper">
-        <!-- Navbar -->
-        @include('layouts.components.navbar')
-        <!-- /.navbar -->
+<div class="wrapper">
+  <!-- Navbar -->
+  @include('layouts.components.navbar')
+  <!-- /.navbar -->
 
-        <!-- Main Sidebar Container -->
-        @include('layouts.components.sidebar')
+  <!-- Main Sidebar Container -->
+  @include('layouts.components.sidebar')
 
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <div class="container-fluid">
-                    @yield('header')
-                </div><!-- /.container-fluid -->
-            </section>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+       @yield('header')
+      </div><!-- /.container-fluid -->
+    </section>
 
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                    @yield('content')
-                </div>
-            </section>
-            <!-- /.content -->
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        @yield('content')
+      </div>
+    </section>
+    <!-- /.content -->
 
-            <a id="back-to-top" href="#" class="btn btn-primary back-to-top" role="button"
-                aria-label="Scroll to top">
-                <i class="fas fa-chevron-up"></i>
-            </a>
-        </div>
-        <!-- /.content-wrapper -->
+    <a id="back-to-top" href="#" class="btn btn-primary back-to-top" role="button" aria-label="Scroll to top">
+      <i class="fas fa-chevron-up"></i>
+    </a>
+  </div>
+  <!-- /.content-wrapper -->
 
-        <footer class="main-footer">
-            <div class="float-right d-none d-sm-block">
-                <b>Version</b> Tester
-            </div>
-            <strong>Copyright &copy; 2025 <a href="#">Admin Cihuy</a>.</strong> All rights reserved.
-        </footer>
-
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-            <!-- Control sidebar content goes here -->
-        </aside>
-        <!-- /.control-sidebar -->
+  <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> Tester
     </div>
-    <!-- ./wrapper -->
+    <strong>Copyright &copy; 2025 <a href="#">Admin Cihuy</a>.</strong> All rights reserved.
+  </footer>
 
-    <!-- jQuery -->
-    <script src="https://aplikasi-invetaris-sederhana.sevalla.app/templates/plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap -->
-    <script src="https://aplikasi-invetaris-sederhana.sevalla.app/templates/plugins/bootstrap/js/bootstrap.bundle.min.js">
-    </script>
-    <!-- AdminLTE -->
-    <script src="https://aplikasi-invetaris-sederhana.sevalla.app/templates/dist/js/adminlte.min.js"></script>
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
 
+<!-- jQuery -->
+<script src="https://aplikasi-invetaris-sederhana.sevalla.app/templates/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="https://aplikasi-invetaris-sederhana.sevalla.app/templates/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE -->
+<script src="https://aplikasi-invetaris-sederhana.sevalla.app/templates/dist/js/adminlte.min.js"></script>
+
+    <!-- 
+
+<script src="{{ asset('templates/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{ asset('templates/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{ asset('templates/dist/js/adminlte.min.js')}}"></script> -->
+</body>
 </html>
