8b13da736a04c9c73df292a4403ace2980844e68
Yusuf Hammam <oksipituaja@gmail.com>
2025-12-03 08:43:11 +0700

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -63,10 +63,16 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
+<script src="https://aplikasi-invetaris-sederhana.sevalla.app/templates/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="https://aplikasi-invetaris-sederhana.sevalla.app/templates/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE -->
+<script src="https://aplikasi-invetaris-sederhana.sevalla.app/templates/dist/js/adminlte.min.js"></script>
+
+    <!-- 
+
 <script src="{{ asset('templates/plugins/jquery/jquery.min.js')}}"></script>
-<!-- Bootstrap 4 -->
 <script src="{{ asset('templates/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<!-- AdminLTE App -->
-<script src="{{ asset('templates/dist/js/adminlte.min.js')}}"></script>
+<script src="{{ asset('templates/dist/js/adminlte.min.js')}}"></script> -->
 </body>
 </html>
