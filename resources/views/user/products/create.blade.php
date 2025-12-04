a82e77742c41afbf59aabbf37095469271e567c8
Yusuf Hammam <oksipituaja@gmail.com>
2025-12-04 15:50:52 +0700

    Update create.blade.php

--- resources/views/user/products/create.blade.php
+++ resources/views/user/products/create.blade.php
@@ -13,6 +13,24 @@
                     @csrf
 
                     <div class="form-group">
+                        <label>Nama Lengkap</label>
+                        <input type="text" name="nama_lengkap" class="form-control" value="{{ old('nama_lengkap') }}">
+                        @error('nama_lengkap')<div class="text-danger">{{ $message }}</div>@enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label>NIM</label>
+                        <input type="text" name="nim" class="form-control" value="{{ old('nim') }}">
+                        @error('nim')<div class="text-danger">{{ $message }}</div>@enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label>Prodi</label>
+                        <input type="text" name="prodi" class="form-control" value="{{ old('prodi') }}">
+                        @error('prodi')<div class="text-danger">{{ $message }}</div>@enderror
+                    </div>
+
+                    <div class="form-group">
                         <label>Nama Barang</label>
                         <input type="text" name="nama_barang" class="form-control" value="{{ old('nama_barang') }}">
                         @error('nama_barang')<div class="text-danger">{{ $message }}</div>@enderror
@@ -36,33 +54,4 @@
 
                     <div class="form-group">
                         <label>Tanggal Selesai</label>
-                        <input type="date" name="tanggal_selesai" class="form-control" value="{{ old('tanggal_selesai') }}">
-                    </div>
-
-                    <div class="form-group">
-                        <label>Kategori</label>
-                        <select name="category_id" class="form-control">
-                            <option value="">-- Pilih Kategori --</option>
-                            @foreach ($categories as $cat)
-                                <option value="{{ $cat->id }}" {{ old('category_id') == $cat->id ? 'selected' : '' }}>
-                                    {{ $cat->nama_barang }}
-                                </option>
-                            @endforeach
-                        </select>
-                        @error('category_id')<div class="text-danger">{{ $message }}</div>@enderror
-                    </div>
-
-                    <div class="form-group">
-                        <label>Stok</label>
-                        <input type="number" name="stok_barang" class="form-control" value="{{ old('stok_barang') }}">
-                        @error('stok_barang')<div class="text-danger">{{ $message }}</div>@enderror
-                    </div>
-
-                    <button type="submit" class="btn btn-primary">Simpan</button>
-                    <a href="{{ route('user.home') }}" class="btn btn-secondary">Batal</a>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+                        <input type="date" name="tanggal_selesai" class="
