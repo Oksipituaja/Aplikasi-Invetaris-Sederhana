0ba1704907fc83c8ebd50cc512192ae5e646f739
Yusuf Hammam <oksipituaja@gmail.com>
2025-12-04 15:47:45 +0700

    Update 2025_07_15_154909_create_products_table.php

--- database/migrations/2025_07_15_154909_create_products_table.php
+++ database/migrations/2025_07_15_154909_create_products_table.php
@@ -4,16 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('nama_lengkap')->nullable();
             $table->string('nim')->nullable();
             $table->string('prodi')->nullable();
@@ -23,13 +18,12 @@ return new class extends Migration
             $table->date('tanggal_mulai');
             $table->date('tanggal_selesai')->nullable();
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Tambahan
+            $table->integer('stok_barang');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('products');
