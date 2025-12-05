0089f83ce4eb1edbd4a2f67904946b1ae326b426
Yusuf Hammam <oksipituaja@gmail.com>
2025-12-05 09:30:56 +0700

    Update 2025_07_23_025529_add_stok_barang_to_products_table.php

--- database/migrations/2025_07_23_025529_add_stok_barang_to_products_table.php
+++ database/migrations/2025_07_23_025529_add_stok_barang_to_products_table.php
@@ -6,15 +6,13 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
             $table->integer('stok_barang')->default(0)->after('tanggal_selesai');
         });
     }
+
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table) {
