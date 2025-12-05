95ce27c8cc1f957fc11e096f9a7298fc596c4c8d
Yusuf Hammam <oksipituaja@gmail.com>
2025-12-05 09:25:24 +0700

    Update 2025_07_15_154909_create_products_table.php

--- database/migrations/2025_07_15_154909_create_products_table.php
+++ database/migrations/2025_07_15_154909_create_products_table.php
@@ -5,6 +5,9 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
@@ -23,6 +26,9 @@ return new class extends Migration {
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('products');
