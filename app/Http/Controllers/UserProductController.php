d60bff4c40b527922743a5c44eb46d831fc19b80
Yusuf Hammam <oksipituaja@gmail.com>
2025-12-05 11:43:00 +0700

    Update UserProductController.php

--- app/Http/Controllers/UserProductController.php
+++ app/Http/Controllers/UserProductController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Models\Category;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 
