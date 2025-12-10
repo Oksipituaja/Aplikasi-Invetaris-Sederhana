5766a264e0d70bca3ded1b91873154073785d3e9
Yusuf Hammam <oksipituaja@gmail.com>
2025-12-10 11:12:27 +0700

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,10 +10,10 @@ class Product extends Model
     protected $fillable = [
         'nama_lengkap','nim','prodi','nama_barang','description',
         'nup_ruangan','tanggal_mulai','tanggal_selesai',
-        'stok_barang','category_id','user_id'
+        'stok_barang','category_id','user_id' // user_id KRUSIAL untuk penyimpanan
     ];
 
-    // FIX UTAMA: Baris ini mewajibkan Laravel mengubah string database jadi objek tanggal
+    // FIX UTAMA: Baris ini mewajibkan Laravel mengubah string database jadi objek tanggal (Carbon)
     protected $casts = [
         'tanggal_mulai' => 'datetime',
         'tanggal_selesai' => 'datetime',
