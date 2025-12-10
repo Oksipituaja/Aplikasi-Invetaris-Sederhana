d51c31f8359a1bb463338eda062c043d9b36438e
Oksipituaja <oksipituaja@gmail.com>
2025-12-11 00:52:47 +0700

    adjust Models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,16 +1,27 @@
 <?php
 
-namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     protected $fillable = [
-        'nama_lengkap','nim','prodi','nama_barang','description',
-        'nup_ruangan','tanggal_mulai','tanggal_selesai',
-        'stok_barang','category_id','user_id' // user_id KRUSIAL untuk penyimpanan
+        // 1. DATA PENANGGUNG JAWAB BARU
+        'user_id', 
+        'nama_lengkap',
+        'nim',
+        'prodi',
+        'phone_number', // <--- WAJIB DITAMBAHKAN
+        'photo_path',   // <--- WAJIB DITAMBAHKAN
+        
+        // 2. DATA BARANG LAMA
+        'nama_barang',
+        'description',
+        'nup_ruangan',
+        'tanggal_mulai',
+        'tanggal_selesai',
+        'stok_barang',
+        'category_id',
     ];
 
     // FIX UTAMA: Baris ini mewajibkan Laravel mengubah string database jadi objek tanggal (Carbon)
